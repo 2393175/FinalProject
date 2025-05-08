58eb887ffe34fe0327304e1fc1b538c88e2de943
2393175 <Naveen.Mallela@cognizant.com>
2025-05-08 23:16:34 +0530

    Initial Commit

---
+++ StaffRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    public class StaffRepository
+    {
+        
+            private readonly AppDbContext _context;
+
+            // Constructor to inject the DbContext
+            public StaffRepository(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            // Create - Add a new staff member
+            public void AddStaff(Staff staff)
+            {
+                _context.Staffs.Add(staff);
+                _context.SaveChanges(); // Commit changes to the database
+            }
+
+            // Read - Get all staff members
+            public List<Staff> GetAllStaff()
+            {
+                return _context.Staffs.ToList();
+            }
+
+            // Read - Get staff member by ID
+            public Staff? GetStaffById(int staffId)
+            {
+                return _context.Staffs.FirstOrDefault(s => s.StaffId == staffId);
+            }
+
+            // Update - Modify an existing staff member
+            public void UpdateStaff(Staff staff)
+            {
+                var existingStaff = _context.Staffs.FirstOrDefault(s => s.StaffId == staff.StaffId);
+                if (existingStaff != null)
+                {
+                    existingStaff.StaffName = staff.StaffName;
+                    existingStaff.StaffRole = staff.StaffRole;
+                    existingStaff.StaffEmail = staff.StaffEmail;
+                    existingStaff.StaffPhoneNumber = staff.StaffPhoneNumber;
+
+                    _context.SaveChanges(); // Commit changes to the database
+                }
+            }
+
+            // Delete - Remove a staff member by ID
+            public void DeleteStaff(int staffId)
+            {
+                var staff = _context.Staffs.FirstOrDefault(s => s.StaffId == staffId);
+                if (staff != null)
+                {
+                    _context.Staffs.Remove(staff);
+                    _context.SaveChanges(); // Commit changes to the database
+                }
+            }
+    }
+}
